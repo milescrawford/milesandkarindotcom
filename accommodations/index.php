8faff3b4ed78c45f24dd924a2f65493bcc1422f3
milescrawford <miles@milescrawford.com>
2016-09-19 19:42:09 -0700

    fill out some info and add rsvp

--- accommodations/index.php
+++ accommodations/index.php
@@ -11,19 +11,17 @@
                     <hr>
                 </div>
                 <div class="col-md-8">
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2692.74860978503!2d-120.68271858437139!3d47.55322127918115!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x549a4e8a646e462d%3A0x32b3f0e56606e794!2sSleeping+Lady+Resort!5e0!3m2!1sen!2sus!4v1474146152119" frameborder="0" style="border:0" allowfullscreen></iframe>
+<p>We would love all our guests to stay at the Sleeping Lady with us, though other options are available. All our events will be held on the grounds.</p>
+<p>All rooms at the resort are held for our guests until late April, 2017. Please make your reservations before then to guarantee that a room will be available.</p>
+<p>Reserving by phone: Call 1-800-574-2123 and indicate that you attending the Crawford/Kubischta wedding, group number #9857.</p>
+<p>Reserving online: Log in to the <a href="https://res.sleepinglady.com/V1WebControls/GroupLogin.aspx">Sleeping Lady Group Reservation Page</a> using group ID 9857 and password 37007816.</p>
+                    <p><strong>Sleeping Lady Resort</strong>
+<br>7375 Icicle Road<br>Leavenworth, WA. 98826
+<br>1-800-574-2123</p>
                 </div>
+
                 <div class="col-md-4">
-                    <p>Phone:
-                        <strong>123.456.7890</strong>
-                    </p>
-                    <p>Email:
-                        <strong><a href="mailto:name@example.com">name@example.com</a></strong>
-                    </p>
-                    <p>Address:
-                        <strong>3481 Melrose Place
-                            <br>Beverly Hills, CA 90210</strong>
-                    </p>
+<iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2692.74860978503!2d-120.68271858437139!3d47.55322127918115!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x549a4e8a646e462d%3A0x32b3f0e56606e794!2sSleeping+Lady+Resort!5e0!3m2!1sen!2sus!4v1474146152119" frameborder="0" style="border:0" allowfullscreen></iframe>
                 </div>
                 <div class="clearfix"></div>
             </div>
@@ -40,7 +38,9 @@
                     <hr>
                     <img class="img-responsive img-border img-left" src="img/tent.jpg" alt="">
                     <hr class="visible-xs">
-<p>Plenty of campgrounds</p>
+<p>The closest camping is <strong>Eightmile Campground</strong>.</p>
+<p>Reservations can be made six months in advance at <a href="http://go.usa.gov/7eKe">Recreation.gov</a>.
+<p>Plenty of other camping options are available in the area, but weekends are very busy, and most everything is first-come-first-serve, so we recommend making a reservation.</p>
                 </div>
             </div>
         </div>
