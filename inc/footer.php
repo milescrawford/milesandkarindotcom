cc8ecd8ed28160974f2cf9c954e30731a742aa08
milescrawford <miles@milescrawford.com>
2016-09-21 18:44:11 -0700

    no carousel or jquery

--- inc/footer.php
+++ inc/footer.php
@@ -8,17 +8,7 @@
         </div>
     </footer>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-    <!-- Script to Activate the Carousel -->
-    <script>
-    $('.carousel').carousel({
-        interval: 5000 //changes the speed
-    })
-    </script>
-
 </body>
 
 </html>
